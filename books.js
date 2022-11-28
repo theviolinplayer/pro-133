371259768048924f1ef5143df14e33bfd6f32589
theviolinplayer <119118863+theviolinplayer@users.noreply.github.com>
2022-11-28 19:45:14 +0530

    Add files via upload

---
+++ books.js
@@ -0,0 +1,40 @@
+img="";
+objects=[];
+status="";
+function preload(){
+    img=loadImage("bookshelf.jpg");
+}
+function setup(){
+    canvas=createCanvas(800, 700);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd", model_has_been_loaded);
+    document.getElementById("status").innerHTML="Status : Detectin' objects"
+}
+function model_has_been_loaded(){
+    console.log("Model's been loaded!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    status="true";
+    objectDetector.detect(img, obtained_the_results)
+}
+function obtained_the_results(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
+}
+function draw(){
+    image(img, 0, 0, 800, 700);
+    if(status!=""){
+        for(x=0; x>objects.length; x++){
+            document.getElementById("status").innerHTML="Status : Detectin' objects";
+            fill("#FF0000");
+            percent=floor(objects[x].confidence*100);
+            text(objects[x].label+" "+percent+"%",objects[x].x+15, objects[x].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[x].x, objects[x].y, objects[x].width, objects[x].height);      
+        }
+    }
+}
